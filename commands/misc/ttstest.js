93065f765edbdbde41eb6c5c8e8effc8cf2ff40e
Wolfyre243 <wolfyreblueflare@gmail.com>
2024-09-28 13:56:27 +0800

    implemented simple audio interface

---
+++ commands/misc/ttstest.js
@@ -0,0 +1,84 @@
+// Import everything
+const { SlashCommandBuilder } = require('discord.js');
+const { endpoint } = require('../../config.json');
+
+const { joinVoiceChannel, getVoiceConnections, createAudioPlayer, AudioPlayerStatus, createAudioResource, generateDependencyReport, VoiceConnectionStatus } = require('@discordjs/voice');
+
+const fs = require('fs');
+const path = require('path');
+const ffmpeg = require('ffmpeg-static');
+
+console.log(generateDependencyReport());
+
+module.exports = {
+    category: 'misc',
+    data: new SlashCommandBuilder()
+            .setName('ttstest') // This should be the same as the file name
+            .setDescription('A command to test TTS'),
+
+    async execute(interaction) {
+        // Fetch audio from server
+        const output = await fetch(`http://${endpoint}:3030/texttospeech`, {
+            method: "POST",
+            body: JSON.stringify({
+                message: "Hello! I am Alyssa, and this is a test output file.",
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        // Parse audio base64 string into a wav file
+        const outputJSON = await output.json();
+        fs.writeFileSync(path.join(__dirname, '../../audio/output.mp3'), Buffer.from(outputJSON.base64String, 'base64'));
+        console.log("file written!");
+
+        const { voice } = interaction.member;
+        console.log(voice.channelId); // shld be 1281188442180554798
+        if (!voice.channelId) {
+            await interaction.reply(`You must be in a voice channel to use this command.`);
+            return;
+        } else if (getVoiceConnections().size > 0) {
+            await interaction.reply(`There is already a voice connection in this server.`);
+            return;
+        }
+        // Set up an audio player
+        const player = createAudioPlayer();
+        player.on(AudioPlayerStatus.Playing, () => {
+            console.log("Playing audio!");
+        });
+
+        player.on('error', (e) => {
+            console.log(`Error: ${e}`);
+        })
+
+        // Join the specified voice channel
+        const voiceConnection = joinVoiceChannel({
+            channelId: voice.channelId,
+            guildId: interaction.guildId,
+            adapterCreator: interaction.guild.voiceAdapterCreator
+        })
+
+        voiceConnection.on(VoiceConnectionStatus.Ready, () => {
+            console.log(`Voice connection established!`);
+            interaction.reply("Successfully connected to voice channel!");
+
+            const audioResource = createAudioResource(path.join(__dirname, '../../audio/output.mp3')); // <- error here
+            player.play(audioResource);
+            console.log("Audio created successfully");
+
+            const voiceSubscription = voiceConnection.subscribe(player);
+            console.log("Subscribed successfully");
+        })
+
+        
+        
+        return;
+
+        // const status = voiceConnection.playOpusPacket(Buffer.from(outputJSON.base64String, 'base64'));
+        // console.log(status);
+        // const audio = voiceConnection.prepareAudioPacket(Buffer.from(outputJSON.base64String, 'base64'));
+        // voiceConnection.dispatchAudio();
+
+        
+    }
+}
